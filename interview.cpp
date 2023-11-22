56d53ee05bb5660c3491eba5a24c9fd8ca065841
Gaurav <gauravsyr10@gmail.com>
2023-11-22 20:17:08 +0530

    Two Sum II - Input Array Is Sorted

--- interview.cpp
+++ interview.cpp
@@ -140,3 +140,54 @@ public:
     return sum == INT_MAX ? 0 : sum;
   }
 };
+
+/*   <<<<<--------------------------  Two Sum II - Input Array Is Sorted -------------------------------------------->>>>>
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+
+*/
+
+class Solution
+{
+public:
+  vector<int> twoSum(vector<int> &numbers, int target)
+  {
+
+    int i = 0, j = numbers.size() - 1;
+    int a, b;
+
+    while (i < j)
+    {
+      int sum = numbers[i] + numbers[j];
+      if (sum == target)
+      {
+        a = i + 1;
+        b = j + 1;
+        break;
+      }
+      else if (sum < target)
+      {
+        i++;
+      }
+      else
+      {
+        j--;
+      }
+    }
+    return {a, b};
+  }
+};
