f4c9b3b2dcfecd79cbc5ad712a5738e90c520090
Gaurav <gauravsyr10@gmail.com>
2023-11-22 19:41:15 +0530

    Minimum Size Subarray Sum

--- interview.cpp
+++ interview.cpp
@@ -99,3 +99,44 @@ public:
     reverse(nums, n, nums.size() - 1);
   }
 };
+
+/*   <<<<<--------------------------  Minimum Size Subarray Sum -------------------------------------------->>>>>
+
+Example 1:
+
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+Example 2:
+
+Input: target = 4, nums = [1,4,4]
+Output: 1
+Example 3:
+
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+
+*/
+
+class Solution
+{
+public:
+  int minSubArrayLen(int target, vector<int> &nums)
+  {
+    int curr_sum = 0;
+    int sum = INT_MAX;
+    int j = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+      curr_sum += nums[i];
+      while (curr_sum >= target)
+      {
+        curr_sum -= nums[j];
+        sum = min(sum, i - j + 1);
+        j++;
+      }
+    }
+
+    return sum == INT_MAX ? 0 : sum;
+  }
+};
