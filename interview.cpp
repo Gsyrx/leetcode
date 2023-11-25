e8b5d36bdfe8cbbb0c92ce67bfdedc0887ec9725
Gaurav <gauravsyr10@gmail.com>
2023-11-25 12:45:09 +0530

    Minimum Equal Sum of Two Arrays After Replacing Zeros

--- interview.cpp
+++ interview.cpp
@@ -329,3 +329,58 @@ public:
     return res + v[0];
   }
 };
+
+/*   <<<<<-------------------------- Minimum Equal Sum of Two Arrays After Replacing Zeros  -------------------------------------------->>>>>
+
+Example 1:
+
+Input: nums1 = [3,2,0,1,0], nums2 = [6,5,0]
+Output: 12
+Explanation: We can replace 0's in the following way:
+- Replace the two 0's in nums1 with the values 2 and 4. The resulting array is nums1 = [3,2,2,1,4].
+- Replace the 0 in nums2 with the value 1. The resulting array is nums2 = [6,5,1].
+Both arrays have an equal sum of 12. It can be shown that it is the minimum sum we can obtain.
+Example 2:
+
+Input: nums1 = [2,0,2,0], nums2 = [1,4]
+Output: -1
+Explanation: It is impossible to make the sum of both arrays equal.
+
+*/
+
+class Solution
+{
+public:
+  long long minSum(vector<int> &nums1, vector<int> &nums2)
+  {
+    long long sum1 = 0, sum2 = 0;
+    int count1 = 0, count2 = 0;
+
+    for (int i = 0; i < nums1.size(); i++)
+    {
+      sum1 += nums1[i];
+      if (nums1[i] == 0)
+      {
+        count1++;
+      }
+    }
+
+    for (int i = 0; i < nums2.size(); i++)
+    {
+      sum2 += nums2[i];
+      if (nums2[i] == 0)
+      {
+        count2++;
+      }
+    }
+
+    if ((count1 == 0 && sum2 + count2 > sum1) || (count2 == 0 && sum1 + count1 > sum2))
+    {
+      return -1;
+    }
+    else
+    {
+      return max(sum1 + count1, sum2 + count2);
+    }
+  }
+};
