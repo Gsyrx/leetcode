0d02a003f7299e7cbfcd95554804cef49a1c6f4f
Gaurav <gauravsyr10@gmail.com>
2023-11-27 19:30:28 +0530

    Product of Array Except Self

--- interview.cpp
+++ interview.cpp
@@ -384,3 +384,43 @@ public:
     }
   }
 };
+
+/*   <<<<<-------------------------- Product of Array Except Self  -------------------------------------------->>>>>
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+*/
+
+class Solution
+{
+public:
+  vector<int> productExceptSelf(vector<int> &nums)
+  {
+    int n = nums.size();
+
+    vector<int> output(n, 1);
+
+    int left_product = 1;
+    for (int i = 0; i < n; ++i)
+    {
+      output[i] *= left_product;
+      left_product *= nums[i];
+    }
+
+    int right_product = 1;
+    for (int i = n - 1; i >= 0; --i)
+    {
+      output[i] *= right_product;
+      right_product *= nums[i];
+    }
+
+    return output;
+  }
+};
