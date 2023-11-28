e803a8923977f29b768e08196ba92237c4540600
Gaurav <gauravsyr10@gmail.com>
2023-11-28 22:18:04 +0530

    Max Number of K-Sum Pairs

--- interview.cpp
+++ interview.cpp
@@ -424,3 +424,55 @@ public:
     return output;
   }
 };
+
+/*   <<<<<-------------------------- Max Number of K-Sum Pairs  -------------------------------------------->>>>>
+
+Example 1:
+
+Input: nums = [1,2,3,4], k = 5
+Output: 2
+Explanation: Starting with nums = [1,2,3,4]:
+- Remove numbers 1 and 4, then nums = [2,3]
+- Remove numbers 2 and 3, then nums = []
+There are no more pairs that sum up to 5, hence a total of 2 operations.
+Example 2:
+
+Input: nums = [3,1,3,4,3], k = 6
+Output: 1
+Explanation: Starting with nums = [3,1,3,4,3]:
+- Remove the first two 3's, then nums = [1,4,3]
+There are no more pairs that sum up to 6, hence a total of 1 operation.
+
+*/
+
+class Solution
+{
+public:
+  int maxOperations(vector<int> &nums, int k)
+  {
+    sort(nums.begin(), nums.end());
+
+    int count = 0, start = 0, end = nums.size() - 1;
+    while (start < end)
+    {
+      int sum = nums[start] + nums[end];
+
+      if (sum == k)
+      {
+        count++;
+        start++;
+        end--;
+      }
+      else if (sum < k)
+      {
+        start++;
+      }
+      else
+      {
+        end--;
+      }
+    }
+
+    return count;
+  }
+};
