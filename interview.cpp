493d4136584e67c2ef805ae82cbc1957146b9a32
Gaurav <gauravsyr10@gmail.com>
2023-11-30 22:44:59 +0530

    Maximum Average Subarray I

--- interview.cpp
+++ interview.cpp
@@ -476,3 +476,39 @@ public:
     return count;
   }
 };
+
+/*   <<<<<-------------------------- Maximum Average Subarray I  -------------------------------------------->>>>>
+
+Example 1:
+
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+Example 2:
+
+Input: nums = [5], k = 1
+Output: 5.00000
+
+*/
+
+class Solution
+{
+public:
+  double findMaxAverage(vector<int> &nums, int k)
+  {
+    int windowSum = 0;
+    for (int i = 0; i < k; i++)
+    {
+      windowSum += nums[i];
+    }
+
+    double maxAvg = (double)windowSum / k;
+    for (int i = k; i < nums.size(); i++)
+    {
+      windowSum += nums[i] - nums[i - k];
+      maxAvg = max(maxAvg, (double)windowSum / k);
+    }
+
+    return maxAvg;
+  }
+};
