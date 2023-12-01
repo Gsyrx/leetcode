c17a640760e842e9aa8d9e1cbc74b130c40a0359
Gaurav <gauravsyr10@gmail.com>
2023-12-01 19:42:14 +0530

    Maximum Number of Vowels in a Substring of Given Length

--- interview.cpp
+++ interview.cpp
@@ -512,3 +512,62 @@ public:
     return maxAvg;
   }
 };
+
+/*   <<<<<-------------------------- Maximum Number of Vowels in a Substring of Given Length  -------------------------------------------->>>>>
+
+Example 1:
+
+Input: s = "abciiidef", k = 3
+Output: 3
+Explanation: The substring "iii" contains 3 vowel letters.
+Example 2:
+
+Input: s = "aeiou", k = 2
+Output: 2
+Explanation: Any substring of length 2 contains 2 vowels.
+Example 3:
+
+Input: s = "leetcode", k = 3
+Output: 2
+Explanation: "lee", "eet" and "ode" contain 2 vowels.
+
+*/
+
+class Solution
+{
+public:
+  bool isVowel(char ch)
+  {
+    return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+  }
+
+  int maxVowels(string s, int k)
+  {
+    int n = s.length();
+    int count = 0;
+
+    for (int i = 0; i < k; i++)
+    {
+      if (isVowel(s[i]))
+      {
+        count++;
+      }
+    }
+    int maxCount = count;
+
+    for (int i = k; i < n; i++)
+    {
+      if (isVowel(s[i]))
+      {
+        count++;
+      }
+      if (isVowel(s[i - k]))
+      {
+        count--;
+      }
+      maxCount = max(maxCount, count);
+    }
+
+    return maxCount;
+  }
+};
